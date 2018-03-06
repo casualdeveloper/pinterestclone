bb09a70e48cc4a8140004a17c4aaccb3ae5bbb30
TOM <tuomis29@gmail.com>
2018-03-06 19:59:28 +0000

    cleaning up, added Button component properties (dark, disabled)

--- src/js/style_components/Button.js
+++ src/js/style_components/Button.js
@@ -2,8 +2,14 @@ import React from "react";
 
 export default class Button extends React.Component {
     render(){
+        let style = "btn ";
+        const { dark, disabled } = this.props;
+
+        if(dark)
+            style += "btn--dark ";
+
         return(
-            <button type="button" className="[ btn dark ]">{this.props.children}</button>
+            <button type="button" disabled={disabled} className={style}>{this.props.children}</button>
         );
     }
 }
