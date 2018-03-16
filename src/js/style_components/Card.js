c830c0c0ffd676d926941b39d5effdb575472c48
TOM <tuomis29@gmail.com>
2018-03-16 21:53:24 +0000

    added Card style and component

---
+++ src/js/style_components/Card.js
@@ -0,0 +1,143 @@
+import React from "react";
+import PropTypes from "prop-types"
+
+const propTypes = {
+    className: PropTypes.string
+}
+
+const defaultProps = {
+    className: ""
+}
+
+export default class Card extends React.Component {
+    render() {
+        let className = "card__body ";
+        className += this.props.className;
+
+        return (
+            <div className={className}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+Card.propTypes = { ...propTypes };
+Card.defaultProps = { ...defaultProps };
+
+
+
+
+
+Card.Title = (props) => {
+    let className = "card__title ";
+    className += props.className;
+
+    return (
+        <div className={className} >
+            {props.children}
+        </div>
+    );
+}
+
+Card.Title.propTypes = { ...propTypes };
+Card.Title.defaultProps = { ...defaultProps };
+
+
+
+
+
+Card.Text = (props) => {
+    let className = "card__text ";
+    className += props.className;
+
+    return (
+        <div className={className} >
+            {props.children}
+        </div>
+    );
+}
+
+Card.Text.propTypes = { ...propTypes };
+Card.Text.defaultProps = { ...defaultProps };
+
+
+
+
+
+Card.Action = (props) => {
+    let className = "card__actions ";
+    className += props.className;
+
+    return (
+        <div className={className}>
+            {props.children}
+        </div>
+    )
+}
+
+Card.Action.propTypes = { ...propTypes };
+Card.Action.defaultProps = { ...defaultProps };
+
+
+
+
+
+Card.Media = (props) => {
+    let className = "card__media ";
+    className += props.className;
+
+    return (
+        <div className={className}>
+            {props.children}
+        </div>
+    )
+}
+
+Card.Media.propTypes = { ...propTypes };
+Card.Media.defaultProps = { ...defaultProps };
+
+
+
+
+
+Card.Header = (props) => {
+    let className = "card__header ";
+    className += props.className;
+
+    return (
+        <div className={className}>
+            {props.children}
+        </div>
+    )
+}
+
+Card.Header.propTypes = { ...propTypes };
+Card.Header.defaultProps = { ...defaultProps };
+
+Card.Header.TextContainer = (props) => {
+    return (
+        <div className="card__header__text-container">
+            {props.children}
+        </div>
+    );
+}
+
+Card.Header.Title = (props) => {
+    return (
+        <div className="card__header__title">
+            {props.children}
+        </div>
+    )
+}
+
+Card.Header.Subtitle = (props) => {
+    return (
+        <div className="card__header__subtitle">
+            {props.children}
+        </div>
+    )
+}
+
+
+export { Card };
