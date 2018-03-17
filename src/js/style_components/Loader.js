6775bb4a02651b0627df4297b526973f0938b14a
TOM <tuomis29@gmail.com>
2018-03-17 11:37:39 +0000

    added light color option, lowered opacity for colors

--- src/js/style_components/Loader.js
+++ src/js/style_components/Loader.js
@@ -4,15 +4,24 @@ import PropTypes from "prop-types";
 export default class Loader extends React.Component {
     render(){
         let style = "";
+        let className = "loader__circle__path ";
 
-        if(this.props.disabled){
+        if(this.props.disabled) {
             style += "display: none;";
+        }
+
+        if(this.props.size) {
+            style += "width: " + this.props.size + ";";
+        }
+
+        if(this.props.light) {
+            className += "light"
         }
 
         return (
             <div className="loader" style={style} >
-                <svg class="loader__circle" viewBox="25 25 50 50">
-                    <circle class="loader__circle__path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10"/>
+                <svg className="loader__circle" viewBox="25 25 50 50">
+                    <circle className={className} cx="50" cy="50" r="20" fill="none" stroke-width="4" stroke-miterlimit="10"/>
                 </svg>
             </div>
         );
@@ -20,7 +29,9 @@ export default class Loader extends React.Component {
 }
 
 Loader.propTypes = {
-    disabled: PropTypes.bool
+    disabled: PropTypes.bool,
+    size: PropTypes.string,
+    light: PropTypes.bool
 }
 
 export { Loader };
