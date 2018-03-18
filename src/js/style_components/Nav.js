abe0b9d83f9138dd31e9128886bb68a0ef9b9579
TOM <tuomis29@gmail.com>
2018-03-18 08:41:03 +0000

    Added modal nav for smaller screens

--- src/js/style_components/Nav.js
+++ src/js/style_components/Nav.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Loader } from "./Loader";
+import { Hamburger } from "./Hamburger";
+import { Modal } from "./Modal";
 
 class NavBar extends React.Component {
     render(){
@@ -27,6 +29,12 @@ NavBar.propTypes = {
     fixedMobile: PropTypes.bool
 }
 
+NavBar.NavBrand = (props) => {
+    return (
+        <div className="title">{props.children}</div>
+    );
+}
+
 NavBar.Loader = (props) => {
     return (
         <div className="my-auto ml-auto">
@@ -43,29 +51,82 @@ class Nav extends React.Component {
     render(){
         const { pullRight } = this.props;
         let style = "";
+
         if(pullRight)
             style += "margin-left: auto;";
+        
         return (
             <nav className="nav" style={style}>
-                <ul>
+                <NavResponsive {...this.props} >
                     {this.props.children}
-                </ul>
+                </NavResponsive>
             </nav>
         );
     }
 }
 
 Nav.propTypes = {
-    pullRight: PropTypes.bool
+    pullRight: PropTypes.bool,
+    responsive: PropTypes.bool
 }
 
+class NavResponsive extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isModalOpen: false
+        }
 
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
 
-Nav.Responsive = (props) => {
-    return (
-        <div className="nav__responsive" >
-        </div>
-    )
+        /**
+         * Create a new set of children with modified onClick
+         * To a new onClick function we attach Modal close function
+         * and previous onClick function if child component had one. 
+         */
+        if(!props.responsive) return;
+
+        this.cildrenWithModifiedOnClick = React.Children.map(this.props.children, (child) => {
+            //if there is onClick already attached make copy of it and attach to a new function
+            let onClickCopy = () => {};
+            if(child.attributes.onClick)
+                onClickCopy = child.attributes.onClick;
+            
+            //attach new onClick to a child
+            child.attributes.onClick = () => { this.closeModal(); onClickCopy() };
+            return child;
+        });
+    }
+
+    openModal() {
+        this.setState({ isModalOpen: true });
+    }
+
+    closeModal() {
+        this.setState({ isModalOpen: false });
+    }
+
+    render(){
+        if(this.props.responsive){
+            return (
+                <ul className="responsive">
+                    {this.props.children}
+                    <NavItem onClick={this.openModal}><Hamburger className="mr-4" /></NavItem>
+                    <Modal open={this.state.isModalOpen} close={this.closeModal}>
+                        <ul className="nav-modal text-center">
+                            {this.cildrenWithModifiedOnClick}
+                        </ul>
+                    </Modal>
+                </ul>
+            );
+        }
+        return (
+            <ul>
+                {this.props.children}
+            </ul>
+        );
+    }
 }
 
 class NavItem extends React.Component {
@@ -77,22 +138,17 @@ class NavItem extends React.Component {
             className += "current ";
 
         return (
-            <li className={className}>{this.props.children}</li>
+            <li onClick={this.props.onClick} className={className}>{this.props.children}</li>
         );
     }
 }
 
 NavItem.propTypes = {
-    active: PropTypes.bool
+    active: PropTypes.bool,
+    onClick: PropTypes.func
 }
 
-class NavBrand extends React.Component {
-    render(){
-        return (
-            <div className="title">{this.props.children}</div>
-        );
-    }
-}
+
 
 export default NavBar;
-export { NavBar, Nav, NavItem, NavBrand };
+export { NavBar, Nav, NavItem };
