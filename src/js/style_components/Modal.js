772c70e1df447e8dc6f2ecd50d9accf3cbaddd62
TOM <tuomis29@gmail.com>
2018-03-20 12:55:13 +0000

    added fade effect

--- src/js/style_components/Modal.js
+++ src/js/style_components/Modal.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Portal from "preact-portal";
 import PropTypes from "prop-types";
+import { CSSTransitionGroup } from "react-transition-group";
 
 const bodyDOM = document.body;
+
+const EnterAnimationTime = 200;
+const LeaveAnimationTime = 200;
+
 
 export default class Modal extends React.Component {
     render() {
@@ -10,10 +15,9 @@ export default class Modal extends React.Component {
         if(!open){
             //remove modal open to allow scrolling again
             bodyDOM.className = bodyDOM.className.replace(/\bmodal-open\b/g, "");
-            return null;
         }
         //attach modal open to body to remove scrollbar
-        if(bodyDOM.className.indexOf("modal-open") === -1)
+        if(open && bodyDOM.className.indexOf("modal-open") === -1)
             bodyDOM.className += "modal-open";
             
         const Close = () => {
@@ -22,17 +26,35 @@ export default class Modal extends React.Component {
                 <span className="modal__close" onClick={close}></span>
             );
         }
-        
+
+        const RenderItem = (
+            (!open)
+            ?null
+            :<div className="modal">
+                <Close />
+                {this.props.children}
+            </div>
+        )
+
         return (
             <Portal into="#modal-root">
-                <div className="modal">
-                    <Close />
-                    {this.props.children}
-                </div>
-                
+                <CSSTransitionGroup component={FirstChild}
+                    transitionName="modal-fade"
+                    transitionAppear={false}
+                    transitionEnter={true}
+                    transitionEnterTimeout={EnterAnimationTime}
+                    transitionLeave={true}
+                    transitionLeaveTimeout={LeaveAnimationTime}>
+                    {RenderItem}
+                </CSSTransitionGroup>
             </Portal>
         );
     }
+}
+
+const FirstChild = (props) => {
+    const childrenArray = React.Children.toArray(props.children);
+    return childrenArray[0] || null;
 }
 
 Modal.propTypes = {
