5eff3a5c4667cfc7ac24a154d2ccc445fd7a6628
TOM <tuomis29@gmail.com>
2018-03-23 18:36:46 +0000

    fixed process.env.node_env detection

--- webpack.config.js
+++ webpack.config.js
@@ -82,6 +82,9 @@ module.exports = {
         } 
     },
     plugins:[
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development")
+        }),
         new CleanWebpackPlugin([PATHS.build]),
         new ExtractTextPlugin({
             filename: (process.env.NODE_ENV === "production") ? "[name].[contenthash].css" : "[name].css",
